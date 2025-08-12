40aaef90a902a50337abdcad82ba18fcc8750733
mohammedd2002 <168116874+mohammedd2002@users.noreply.github.com>
2025-08-12 17:29:58 +0300

    database seeder

---
+++ database/factories/ReviewsFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ReviewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'user_id' => User::where('role', 'learner')->inRandomOrder()->first()->id,
+            'course_id' => Course::inRandomOrder()->first()->id,
+            'rating' => $rating = fake()->numberBetween(1, 5),
+           'review' => fake()->paragraph(),
+            'status' => $rating < 3 ? 'Reported' : 'Normal',
+        ];
+    }
+}
